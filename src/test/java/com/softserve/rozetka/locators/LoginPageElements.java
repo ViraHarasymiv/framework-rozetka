5e8a0efbda12610734dcd97dd7b4baf6a60c8fda
Dmytro Savchenko <dmitriysavchenko11@gmail.com>
2022-11-23 18:10:04 +0200

    first login test additional assert(border color)

--- src/test/java/com/softserve/rozetka/locators/LoginPageElements.java
+++ src/test/java/com/softserve/rozetka/locators/LoginPageElements.java
@@ -6,11 +6,11 @@ public class LoginPageElements {
     private static By emailAndPhoneField = By.xpath("//*[@id=\"auth_email\"]");
     private static By passwordField = By.xpath("//*[@id=\"auth_pass\"]");
     private static By loginButton = By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[5]/button[1]");
-    private static By errorEmailOrPhone = By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[1]/p");
+    private static By errorEmailOrPhoneField = By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[1]/p");
     private static By typeOfEmailOrPhoneField = By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[1]");
     public static By getEmailAndPhoneField(){return emailAndPhoneField;}
     public static By getPasswordField(){return LoginPageElements.passwordField;}
-    public static By getErrorInvalidEmailOrPhone(){return LoginPageElements.errorEmailOrPhone;}
+    public static By getErrorInvalidEmailOrPhoneField(){return LoginPageElements.errorEmailOrPhoneField;}
     public static By getLoginButton(){return loginButton;}
     public static By getTypeOfEmailOrPhoneField(){return typeOfEmailOrPhoneField;}
 }
--- src/test/java/com/softserve/rozetka/tests/LoginPageTest.java
+++ src/test/java/com/softserve/rozetka/tests/LoginPageTest.java
@@ -11,14 +11,16 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.Color;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LoginPageTest extends BaseRunner {
-    private static final String INVALID_EMAIL_OR_PHONE_MSG  = "Введено невірну адресу ел. пошти або номер телефону";
+    private static final String INVALID_EMAIL_OR_PHONE_MESSAGE  = "Введено невірну адресу ел. пошти або номер телефону";
     private static final String TYPE_OF_EMAIL_PHONE_FIELD  = "form__row validation_type_error";
+    private static final String[] BORDER_COLOR_EMAIL_FIELD  = new String[]{"248", "65", "71", "1"};
     @BeforeClass
     public void setPreconditions() {
         setDriver();
@@ -30,7 +32,11 @@ public class LoginPageTest extends BaseRunner {
         new LoginPage(driver)
                 .enterInvalidEmail(LoginPageElements.getEmailAndPhoneField())
                 .clickOnPasswordField(LoginPageElements.getPasswordField());
-        Assert.assertEquals(driver.findElement(LoginPageElements.getErrorInvalidEmailOrPhone()).getText(), INVALID_EMAIL_OR_PHONE_MSG);
+        Assert.assertEquals(driver.findElement(LoginPageElements.getErrorInvalidEmailOrPhoneField()).getText(), INVALID_EMAIL_OR_PHONE_MESSAGE);
         Assert.assertEquals(driver.findElement(LoginPageElements.getTypeOfEmailOrPhoneField()).getAttribute("class"), TYPE_OF_EMAIL_PHONE_FIELD);
+        String[] rgb = driver.findElement(LoginPageElements.getErrorInvalidEmailOrPhoneField()).getCssValue("border-bottom-color").replaceAll("(rgba)|(rgb)|(\\()|(\\s)|(\\))","").split(",");
+        for(int i = 0; i < rgb.length ;i++){
+            Assert.assertEquals(rgb[i], BORDER_COLOR_EMAIL_FIELD[i]);
+        }
     }
 }
