c18c58b25b463cc5f990a92a26f6168659122434
Dmytro Savchenko <dmitriysavchenko11@gmail.com>
2022-12-15 16:28:18 +0200

    Error locators com.softserve.rozetka.locators.login_page_locators

--- src/main/java/com/softserve/rozetka/locators/login_page_locators/LoginPageLocators.java
+++ src/main/java/com/softserve/rozetka/locators/login_page_locators/LoginPageLocators.java
@@ -5,20 +5,19 @@ import org.openqa.selenium.By;
 public enum LoginPageLocators {
 
     //Доробити Локатори
-    NOTEBOOKS_AND_COMPUTERS_CATEGORY(By.xpath("//li[contains(@class, 'menu-categories__item m')]/a[contains(@href, 'c80253/')]")),
-    MENU_WRAPPER(By.xpath("//div[contains(@class, 'menu-wrapper')]"));
+    EMAIL_OR_PHONE_FIELD(By.xpath("//*[@id=\"auth_email\"]")),
+    PASSWORD_FIELD(By.xpath("//*[@id=\"auth_pass\"]"));
+    LOGIN_BUTTON(By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[5]/button[1]"));
+    INVALID_EMAIL_OR_PHONE_FIELD(By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[1]/p"));
+    TYPE_OF_EMAIL_OR_PHONE_FIELD(By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[1]"));
+    SIGNIN_BUTTON(By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[5]/button[2]"));
+    REMIND_PASSWORD_BUTTON(By.xpath("/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[3]/a"));
+    HIDE_SHOW_PASSWORD_BUTTON(By.cssSelector("body > app-root > rz-single-modal-window > div.modal__holder.modal__holder_show_animation.modal__holder--medium > div.modal__content > rz-user-identification > rz-auth > div > form > fieldset > div:nth-child(2) > div > div > button > svg > use"));
 
     private final By path;
 
     LoginPageLocators(By path) {
         this.path = path;
     }
-
-    public By getPath() {
-        return path;
-    }
-
-
-
 
 }
