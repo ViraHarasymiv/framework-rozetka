01b50c4e71c7ba3a6e9c37b736fa3d6b90045cf6
Vira Harasymiv <vira.harasymiv@nung.edu.ua>
2022-12-22 11:15:16 +0200

    add multiple items to cart test is changed

--- src/main/java/com/softserve/rozetka/pages/notebooks_page/NotebooksPage.java
+++ src/main/java/com/softserve/rozetka/pages/notebooks_page/NotebooksPage.java
@@ -47,10 +47,8 @@ public class NotebooksPage extends ProductsPage {
     @Step("Click on the multiple notebook's cart icons in the Notebooks Page")
     public HeaderComponent putMultipleProductsToCart(){
         waitForElementsToAppear(getProductItems().getCartIcons());
-           for (int i = 1; i < 4; i++){
-               waitForElementToAppear(getProductItems().getCartIcons().get(i));
-               waitForElementBecomeClickable(getProductItems().getCartIcons().get(i)).click();
-               waitForTextToBePresent(getProductItems().getCartIcons().get(i),String.valueOf(i));
+           for (int i = 0; i < 3; i++){
+              waitForElementBecomeClickable(getProductItems().getCartIcons().get(i)).click();
            }
         return getHeaderComponent();
     }
